c8f85da017c67a380dd06ef1777058bbca8d9fc6
Lokesh Yadav <lokesh.yadav1994@yahoo.com>
2018-02-03 14:44:57 +0530

    Add files via upload

---
+++ src/pages/index.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+export default () =>
+ <div style={{ color: `tomato` }}>
+   <h1>Red Carpet</h1>
+   <p>Welcome To Redcarpet.</p>
+   <button onClick={() => this.setState({ count: this.state.count +
+          5 })}>About
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}>Campus Leaders
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}>Contact
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}>FAQs
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}>Jobs
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}>Shop
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}>Smart-Card
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}>The RC Blog
+        </button>
+        <button onClick={() => this.setState({ count: this.state.count -
+          1 })}>Videos
+        </button>
+   <img src="https://www.redcarpetup.com/comsite/assets/images/indexImages/BannerImage.svg" alt="" />
+ </div>
